af66e070c4178154b136938ec84b31c3f719d93b
Scott Atkinson <scott.atkin79@gmail.com>
2022-04-03 14:54:05 +0000

    create admin

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar-scott.herokuapp.com', 'local_host']
+ALLOWED_HOSTS = ['codestar-scott.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
